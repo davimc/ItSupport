394c4908ed47aa4b16e877c8fa6780d682134641
davimc <davimatosc@hotmail.com>
2024-01-02 06:23:26 -0300

    standardization

--- src/main/java/br/com/github/davimc/ItSupport/services/exceptions/ObjectNotFoundException.java
+++ src/main/java/br/com/github/davimc/ItSupport/services/exceptions/ObjectNotFoundException.java
@@ -3,6 +3,6 @@ package br.com.github.davimc.ItSupport.services.exceptions;
 public class ObjectNotFoundException extends RuntimeException{
 
     public ObjectNotFoundException(Long id, Class clazz) {
-        super("Id: "+ id + " não encontrado para a classe " + clazz.getSimpleName());
+        super("Id: "+ id + " not found for"+ clazz.getSimpleName()+ " class ");
     }
 }
