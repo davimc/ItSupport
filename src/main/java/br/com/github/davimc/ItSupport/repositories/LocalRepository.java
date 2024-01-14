4aabc9ed1d04567de0ff69e06968d467f949b3c5
davimc <davimatosc@hotmail.com>
2024-01-14 13:45:59 -0300

    fix: @Repository

--- src/main/java/br/com/github/davimc/ItSupport/repositories/LocalRepository.java
+++ src/main/java/br/com/github/davimc/ItSupport/repositories/LocalRepository.java
@@ -2,9 +2,11 @@ package br.com.github.davimc.ItSupport.repositories;
 
 import br.com.github.davimc.ItSupport.entities.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface LocalRepository extends JpaRepository<Local, UUID> {
 
 }
