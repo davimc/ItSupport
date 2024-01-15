27cfdee76c771e67ed7b5c05352dec94a6f04057
davimc <davimatosc@hotmail.com>
2024-01-14 23:33:28 -0300

    update: contacts

--- src/main/java/br/com/github/davimc/ItSupport/entities/User.java
+++ src/main/java/br/com/github/davimc/ItSupport/entities/User.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -26,6 +27,8 @@ public class User {
     //TODO cpf e cnpj
     @Column(unique = true)
     private String cpf;
-
     private String password;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+    private List<Contact> contacts = new ArrayList<>();
 }
