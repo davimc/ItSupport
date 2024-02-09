559c9ef39e91f18ca2c8736b6d6cb7adedab4924
davimc <davimatosc@hotmail.com>
2024-02-09 00:05:43 -0300

    fix: Integer

--- src/main/java/br/com/github/davimc/ItSupport/entities/enums/DeviceType.java
+++ src/main/java/br/com/github/davimc/ItSupport/entities/enums/DeviceType.java
@@ -7,11 +7,20 @@ public enum DeviceType {
     PRINTER(1, "Impressora"), PC(2, "PC"),
     CELPHONE(3,"Smartphone"), OTHERS(4, "Outros");
 
-    private final int cod;
+    private final Integer cod;
     private final String desc;
 
-    DeviceType(int cod, String desc) {
+    DeviceType(Integer cod, String desc) {
         this.cod = cod;
         this.desc = desc;
     }
+
+    public static DeviceType toEnum(Integer cod){
+        if(cod == null) return null;
+        for(DeviceType x: DeviceType.values())
+            if(cod.equals(x.getCod()))
+                return x;
+        throw new IllegalArgumentException("Tipo inválido");
+    }
+
 }
