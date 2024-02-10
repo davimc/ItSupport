2de0aa2bd5b3f8e533a17b3657e2db33ef79dbab
davimc <davimatosc@hotmail.com>
2024-02-10 09:41:50 -0300

    update: findAll by list id

--- src/main/java/br/com/github/davimc/ItSupport/services/DeviceService.java
+++ src/main/java/br/com/github/davimc/ItSupport/services/DeviceService.java
@@ -3,6 +3,7 @@ package br.com.github.davimc.ItSupport.services;
 import br.com.github.davimc.ItSupport.dto.device.DeviceDTO;
 import br.com.github.davimc.ItSupport.dto.device.DeviceNewDTO;
 import br.com.github.davimc.ItSupport.entities.Device;
+import br.com.github.davimc.ItSupport.entities.User;
 import br.com.github.davimc.ItSupport.repositories.DeviceRepository;
 import br.com.github.davimc.ItSupport.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +11,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class DeviceService {
     @Autowired
     private DeviceRepository repository;
+
+    @Autowired
+    private UserService userService;
 
     protected Device find(UUID id) {
         return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException(id, Device.class));
@@ -26,12 +33,18 @@ public class DeviceService {
 
         return new DeviceDTO(obj);
     }
+
+    protected Set<Device> findAll(List<UUID> devicesId) {
+        return devicesId.stream().map(this::find).collect(Collectors.toSet());
+    }
     public Page<DeviceDTO> findAll(Pageable pageable) {
         return repository.findAll(pageable).map(DeviceDTO::new);
     }
 
-    public DeviceDTO create(DeviceNewDTO newDTO){
-        Device obj = newDTO.copyToEntity();
+    public DeviceDTO create(DeviceNewDTO dto){
+        Device obj = dto.copyToEntity();
+        User u = userService.find(dto.getOwnerId());
+        obj.setUser(u);
         obj = repository.save(obj);
 
         return new DeviceDTO(obj);
