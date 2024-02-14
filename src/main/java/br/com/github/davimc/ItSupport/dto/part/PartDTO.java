6ef7d19fb6fa75ef41d395bd07f5054e9ec95d79
davimc <davimatosc@hotmail.com>
2024-02-14 13:39:28 -0300

    fix: part name

--- src/main/java/br/com/github/davimc/ItSupport/dto/part/PartDTO.java
+++ src/main/java/br/com/github/davimc/ItSupport/dto/part/PartDTO.java
@@ -4,9 +4,9 @@ import br.com.github.davimc.ItSupport.entities.Part;
 
 import java.util.UUID;
 
-public record PartDTO(UUID id, double price, Double percentage, double salePrice, Integer quantity, String local) {
+public record PartDTO(UUID id, String name, double price, Double percentage, double salePrice, Integer quantity, String local) {
     public PartDTO (Part obj){
-        this(obj.getId(), obj.getPrice(), (obj.getPercentageSale()*100),
+        this(obj.getId(), obj.getName(), obj.getPrice(), (obj.getPercentageSale()*100),
                 obj.getPrice() + (obj.getPrice()*obj.getPercentageSale()), obj.getQuantity(),
                 obj.getLocal().getName());
     }
