f4a6c8ce220f3a301de690eb8ab1193717094da7
davimc <davimatosc@hotmail.com>
2024-02-14 10:43:46 -0300

    update: equals, hashcode

--- src/main/java/br/com/github/davimc/ItSupport/entities/Device.java
+++ src/main/java/br/com/github/davimc/ItSupport/entities/Device.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -34,4 +35,17 @@ public class Device {
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private User user;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Device device = (Device) o;
+        return id.equals(device.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
