5cad92d18fa7e43c77c75992f6955f75d51a8c81
davimc <davimatosc@hotmail.com>
2024-10-19 19:16:20 -0300

    Adding device client name

--- src/main/java/br/com/github/davimc/ItSupport/dto/device/DeviceDTO.java
+++ src/main/java/br/com/github/davimc/ItSupport/dto/device/DeviceDTO.java
@@ -9,9 +9,9 @@ import jakarta.persistence.Enumerated;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record DeviceDTO(UUID id, LocalDateTime createdAt,String type, String brand,String model, String characteristics,String obs)
+public record DeviceDTO(UUID id, LocalDateTime createdAt,String type, String brand,String model, String characteristics,String obs, String clientName)
 {
     public DeviceDTO(Device obj){
-        this(obj.getId(),obj.getCreatedAt(), obj.getType().getDesc(),obj.getBrand(), obj.getModel(), obj.getCharacteristics(), obj.getObs());
+        this(obj.getId(),obj.getCreatedAt(), obj.getType().getDesc(),obj.getBrand(), obj.getModel(), obj.getCharacteristics(), obj.getObs(), obj.getUser().getName());
     }
 }
