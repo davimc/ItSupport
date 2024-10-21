d835c3e9ad5aac4136d1af037e8dab391c98ff79
davimc <davimatosc@hotmail.com>
2024-10-21 18:12:24 -0300

    Job details (job + devices id + technician id)

--- src/main/java/br/com/github/davimc/ItSupport/repositories/JobRepository.java
+++ src/main/java/br/com/github/davimc/ItSupport/repositories/JobRepository.java
@@ -1,5 +1,6 @@
 package br.com.github.davimc.ItSupport.repositories;
 
+import br.com.github.davimc.ItSupport.dto.job.JobDetailsDTO;
 import br.com.github.davimc.ItSupport.dto.job.JobTotalDTO;
 import br.com.github.davimc.ItSupport.entities.Job;
 import org.springframework.data.domain.Page;
@@ -8,24 +9,38 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface JobRepository extends JpaRepository<Job, UUID> {
 //todo está errado a forma de chamar o tb_tasks, tem que comparar o id
-   // todo fazer join com usuário para que busque o cliente e técnico
+    // todo fazer join com usuário para que busque o cliente e técnico
 
-@Query("SELECT new br.com.github.davimc.ItSupport.dto.job.JobTotalDTO( " +
-        "    j.id, j.os, j.status, j.createdAt, j.type, j.finishedAt,  c.name, tc.name, " +
-        "    COALESCE(SUM(t.amount), 0)) " +
-        "FROM Job j "+
-        "LEFT JOIN j.tasks t " +
-        "INNER JOIN j.devices d " +
-        "INNER JOIN d.user c " +
-        "INNER JOIN j.tech tc " +
-        "GROUP BY j.os, j.status, j.createdAt, j.type, j.finishedAt, tc.name, c.name")
-   Page<JobTotalDTO> findAllJobView(Pageable pageable);
+    @Query("SELECT new br.com.github.davimc.ItSupport.dto.job.JobTotalDTO( " +
+            "    j.id, j.os, j.status, j.createdAt, j.type, j.finishedAt,  c.name, tc.name, " +
+            "    COALESCE(SUM(t.amount), 0)) " +
+            "FROM Job j " +
+            "LEFT JOIN j.tasks t " +
+            "INNER JOIN j.devices d " +
+            "INNER JOIN d.user c " +
+            "INNER JOIN j.tech tc " +
+            "GROUP BY j.os, j.status, j.createdAt, j.type, j.finishedAt, tc.name, c.name")
+    Page<JobTotalDTO> findAllJobView(Pageable pageable);
+
+    @Query("SELECT new br.com.github.davimc.ItSupport.dto.job.JobDetailsDTO(" +
+            "j.id, j.status, j.type, j.createdAt, j.finishedAt," +
+            "GROUP_CONCAT(d.id) AS devicesId, " +
+            "u.id AS techId) " +
+            "FROM Job j " +
+            "INNER JOIN j.devices d " +
+            "INNER JOIN j.tech u " +
+            "WHERE j.id = :id")
+    Optional<JobDetailsDTO> findJobDetails(UUID id);
+
 }
+
+
 
 /*
 SELECT  " +
