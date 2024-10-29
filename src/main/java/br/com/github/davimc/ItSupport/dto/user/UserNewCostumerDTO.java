bcbfd525b4c9611ee31ffb4ee55db837055daa27
davimc <davimatosc@hotmail.com>
2024-10-29 20:38:36 -0300

    provisional: birthdate formatter

--- src/main/java/br/com/github/davimc/ItSupport/dto/user/UserNewCostumerDTO.java
+++ src/main/java/br/com/github/davimc/ItSupport/dto/user/UserNewCostumerDTO.java
@@ -8,17 +8,19 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 
 public record UserNewCostumerDTO(@NotNull(message = "Name cannot be null") String name, @NotNull(message = "Email cannot be null") @Email(message = "Invalid email format") String email,
                                  @NotNull(message = "Document cannot be null") String document,
-                                 Integer type, String tel, LocalDate birthdate,
+                                 Integer type, String tel, String birthdate,
                                  @NotNull(message = "CEP cannot be null") String cep, String street, Integer number, String district, String city, @Size(min = 2,max = 2, message = "Just the acronym for the state") String state) {
 
     public User copyToEntity() {
-
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         //todo lembrar de corrigir password
-        return new User(name, email, "$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG", "", birthdate, null, null);
+        return new User(name, email, "$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG", "", LocalDate.parse(birthdate, formatter), null, null);
     }
 }
