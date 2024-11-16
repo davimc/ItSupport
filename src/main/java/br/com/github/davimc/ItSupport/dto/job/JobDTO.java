3e2b6d389efd418a34c604510628eea624ff1349
davimc <davimatosc@hotmail.com>
2024-11-15 23:48:05 -0300

    update: user to owner

--- src/main/java/br/com/github/davimc/ItSupport/dto/job/JobDTO.java
+++ src/main/java/br/com/github/davimc/ItSupport/dto/job/JobDTO.java
@@ -1,17 +1,15 @@
 package br.com.github.davimc.ItSupport.dto.job;
 
 import br.com.github.davimc.ItSupport.entities.Job;
-import br.com.github.davimc.ItSupport.entities.enums.JobType;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 public record JobDTO(UUID id, int os, String clientName, String status, String type, String tech, LocalDateTime createdAt, LocalDateTime finishedAt)
 {
     public JobDTO(Job obj){
         this(obj.getId(), obj.getOs(),
-                obj.getDevices().stream().findFirst().get().getUser().getName(),
+                obj.getDevices().stream().findFirst().get().getOwner().getName(),
                 obj.getStatus().getDesc(),
                 obj.getType().getDesc(), obj.getTech().getName(),
                 obj.getCreatedAt(), obj.getFinishedAt());
